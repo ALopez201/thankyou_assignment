8396feae1baabfb71f25ada0274b5fc6904a73a1
ALopez201 <al234@njit.edu>
2019-08-05 20:41:21 -0400

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1>Hello, world!</h1>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+@endsection
