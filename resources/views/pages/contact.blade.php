a1110a12ca6e58a1fe6c7a300d45ecdb51bf69ac
ALopez201 <al234@njit.edu>
2019-08-06 01:24:17 -0400

    added dd feature for accepting data to best of my ability.

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,15 +6,17 @@
     <h1>Anthony's Contacts Page</h1>
     <p class="lead">Use this document as a way to reach out.<br> All you have to do is leave me a message.</p>
 
-    <form>
+    <form action="/contact" method="post">
+        @csrf
         <div class="form-group">
-            <label for="Email">Email address</label>
-            <input type="email" class="form-control" id="Email" placeholder="name@example.com">
+            <label for="email">Email address</label>
+            <input name="email" class="form-control" id="email" placeholder="name@example.com">
+
         </div>
 
         <div class="form-group">
             <label for="body">Message</label>
-            <textarea name= "body" class="form-control" id="body" rows="3"></textarea>
+            <textarea name="body" class="form-control" id="body" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-primary mb-2">Post message</button>
     </form>
